42ca4c817f5afa0b55536941ef5e645b2f3cf1c0
pmm0316 <847679250@qq.com>
2019-01-19 14:29:45 +0800

    完成顶部面板

--- 03-城堡决斗/js/state.js
+++ 03-城堡决斗/js/state.js
@@ -1,6 +1,6 @@
 // Some usefull variables
-var maxHealth = 10
-var maxFood = 10
+var maxHealth = 10 // 最大生命值
+var maxFood = 10 // 最多食物
 var handSize = 5
 var cardUid = 0
 var currentPlayingCard = null
@@ -9,5 +9,17 @@ var currentPlayingCard = null
 var state = {
   // World
   worldRatio: getWorldRatio(),
-  // TODO Other things
+  // 回合数
+  turn: 1,
+  // 玩家
+  players: [
+    {
+      name: 'Anne of Cleves'
+    },
+    {
+      name: 'William the Bald'
+    }
+  ],
+  // 当前玩家的索引
+  currentPlayerIndex: Math.round(Math.random())
 }
--- 03-城堡决斗/main.js
+++ 03-城堡决斗/main.js
@@ -1,4 +1,17 @@
 new Vue({
   name: 'game',
-  el: '#app'
+  el: '#app',
+  data: state,
+  template: `
+    <div id="app">
+       <top-bar
+       :players="players"
+       :current-player-index="currentPlayerIndex"
+       :turn="turn"/>
+    </div>
+  `
+})
+// 监听窗口的大小变化
+window.addEventListener('resize', () => {
+  state.worldRatio = getWorldRatio()
 })
