7efb89d9a4a42e9a0bf43c792504ad4acb5cd6e1
pmm0316 <847679250@qq.com>
2019-01-21 14:19:41 +0800

    添加state.js，保存用户信息

--- support-center/src/main.js
+++ support-center/src/main.js
@@ -3,13 +3,17 @@ import AppLayout from './components/AppLayout'
 import router from './router'
 import './global-components'
 import VueFetch from './plugins/fetch'
+import state from './state'
+import VueState from './plugins/state'
 
 Vue.use(VueFetch, {
   baseUrl: 'https://www.easy-mock.com/mock/5bfb6f42db1e494ccae9b098'
 })
+Vue.use(VueState, state)
 
 new Vue({
   el: '#app',
   render: h => h(AppLayout),
-  router
+  router,
+  data: state
 })
